3dc9c997fc66e9debcc1c91a6e6aebeb31f00958
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-06-15 00:30:38 +0000

    formating

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,22 +3,44 @@ from  bs4 import BeautifulSoup
 import json
 
 
-def get_citations_needed_count(url):
+import requests
+from bs4 import BeautifulSoup
 
+def get_citations_needed_count(url):
+    """
+    Retrieves the number of 'citation needed' occurrences on a web page.
+
+    Args:
+        url (str): The URL of the web page to parse.
+
+    Returns:
+        int: The count of 'citation needed' occurrences on the web page.
+    """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('span', string="citation needed")
     return len(results)
 
-def get_citations_needed_report(url):
 
-    string =""
+def get_citations_needed_report(url):
+    """
+    Retrieves the full text surrounding each 'citation needed' occurrence on a web page.
+
+    Args:
+        url (str): The URL of the web page to parse.
+
+    Returns:
+        str: The report containing the full text surrounding each 'citation needed' occurrence,
+        separated by newlines ('\n').
+    """
+    string = ""
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('span', string="citation needed")
-    for res in results :
-        string += (res.parent.parent.parent.parent.text + "\n")
-    return "\n" + string
+    for res in results:
+        string += ( "\n" + res.parent.parent.parent.parent.text )
+    return string
+
 
 
 
